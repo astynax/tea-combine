fa1444db6d28a4a3e923265e1660c8f9eb0f6c3b
Aleksey Pirogov <astynax@users.noreply.github.com>
2017-11-05 16:27:39 +0300

    restyle wrapView helper in examples/Utils

--- examples/Utils.elm
+++ examples/Utils.elm
@@ -1,14 +1,37 @@
 module Utils exposing (..)
 
 import Html exposing (Html)
+import Html.Attributes exposing (style)
 
 
 wrapView : String -> (model -> Html msg) -> model -> Html msg
 wrapView title view model =
-    Html.div []
-        [ Html.h3 [] [ Html.text title ]
-        , view model
-        , Html.pre []
+    Html.div
+        [ style
+            [ ( "margin", "5px 5px" )
+            , ( "border", "1px solid lightgray" )
+            ]
+        ]
+        [ Html.h3
+            [ style
+                [ ( "margin", "0" )
+                , ( "background-color", "skyblue" )
+                ]
+            ]
+            [ Html.text title ]
+        , Html.div
+            [ style
+                [ ( "margin", "10px 10px" )
+                ]
+            ]
+            [ view model ]
+        , Html.pre
+            [ style
+                [ ( "margin", "0" )
+                , ( "padding", "3px 3px" )
+                , ( "background-color", "lightgray" )
+                ]
+            ]
             [ Html.text "model = "
             , Html.text <| toString model
             ]
